4c0b7db62f53cac9b8bc87e3637fc73f8f32d2c5
Richard Oden <richard.thomas.oden@gmail.com>
2023-06-01 07:31:45 -0500

    Added solutions for Designing a Library  exercise.

--- src/bin/designing_a_library.rs
+++ src/bin/designing_a_library.rs
@@ -25,28 +25,32 @@ impl Book {
 // - `self` for unique access by value.
 impl Library {
     fn new() -> Library {
-        todo!("Initialize and return a `Library` value")
+        Library {
+            books: Vec::<Book>::new()
+        }
     }
 
-    //fn len(self) -> usize {
-    //    todo!("Return the length of `self.books`")
-    //}
+    fn len(&self) -> usize {
+       self.books.len()
+    }
 
-    //fn is_empty(self) -> bool {
-    //    todo!("Return `true` if `self.books` is empty")
-    //}
+    fn is_empty(&self) -> bool {
+       self.len() == 0
+    }
 
-    //fn add_book(self, book: Book) {
-    //    todo!("Add a new book to `self.books`")
-    //}
+    fn add_book(&mut self, book: Book) {
+       self.books.push(book)
+    }
 
-    //fn print_books(self) {
-    //    todo!("Iterate over `self.books` and each book's title and year")
-    //}
+    fn print_books(&self) {
+        for book in &self.books {
+            println!("- {} ({})", book.title, book.year);
+        }
+    }
 
-    //fn oldest_book(self) -> Option<&Book> {
-    //    todo!("Return a reference to the oldest book (if any)")
-    //}
+    fn oldest_book(&self) -> Option<&Book> {
+        self.books.iter().min_by_key(|book| book.year)
+    }
 }
 
 // This shows the desired behavior. Uncomment the code below and
@@ -54,23 +58,22 @@ impl Library {
 // method signatures, including the "self" parameter! You may
 // also need to update the variable bindings within main.
 fn main() {
-    let library = Library::new();
+    let mut library = Library::new();
 
-    //println!("The library is empty: {}", library.is_empty());
-    //
-    //library.add_book(Book::new("Lord of the Rings", 1954));
-    //library.add_book(Book::new("Alice's Adventures in Wonderland", 1865));
-    //
-    //println!("The library is no longer empty: {}", library.is_empty());
-    //
-    //
-    //library.print_books();
-    //
-    //match library.oldest_book() {
-    //    Some(book) => println!("The oldest book is {}", book.title),
-    //    None => println!("The library is empty!"),
-    //}
-    //
-    //println!("The library has {} books", library.len());
-    //library.print_books();
+    println!("The library is empty: {}", library.is_empty());
+    
+    library.add_book(Book::new("Lord of the Rings", 1954));
+    library.add_book(Book::new("Alice's Adventures in Wonderland", 1865));
+    
+    println!("The library is no longer empty: {}", library.is_empty());
+    
+    library.print_books();
+    
+    match library.oldest_book() {
+       Some(book) => println!("The oldest book is {}", book.title),
+       None => println!("The library is empty!"),
+    }
+    
+    println!("The library has {} books", library.len());
+    library.print_books();
 }
